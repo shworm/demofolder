a34f1a85738e7853b8a74a332e9cd4cd64e1889e
shworm <volkvegaming@gmail.com>
2024-05-18 16:44:07 -0600

    base template creation, home html created as template, urls added for home page, and views for app added render to home function!

--- demo/demo_dj/demo_dj/settings.py
+++ demo/demo_dj/demo_dj/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import numpy
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
